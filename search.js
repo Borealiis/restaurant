92dd787b64e687b37a1f78b7693eb243a4023321
MaximeNef <maximenef1@gmail.com>
2020-12-09 18:11:27 +0100

    projet fini sauf readme

--- search.js
+++ search.js
@@ -1,136 +1,99 @@
-const menu = [
-    {
-        image: "./assets/menu/cashew-chickenNoodles.png",
-        name:  "Cashew Chicken noodles",
-        price: "8.50€",
-        description: "Sautéed chicken with asian sauce, cashew, vegetables and noodles.",
-        tag: "lunch"
-    },
-    {
-        image: "./assets/menu/soy-sauce-chicken-cn.jpg",
-        name: "See Yao Gai (soy sauce chicken)",
-        price: "9€",
-        description: "Soy sauce chicken with rice, ginger, scallions, and lightly seasoned vegetables.",
-        tag: "lunch"
-    },
-    {
-        image: "./assets/menu/chicken-yakisoba.jpg",
-        name: "Chicken Yakisoba Noodle Bowl",
-        price: "8.20€",
-        description: "Soba noodles with chicken, shredded carrots and cabbage.",
-        tag: "most popular"
-    },
-    {
-        image: "./assets/menu/tsukemen-noodle.jpg",
-        name: "Tsukemen (dripping ramen noodles)",
-        price: "11€",
-        description: "Cold ramen noodles with a separate bowl of broth, pork, shrimps, hard-boiled egg, nori, ginger, asian mushrooms, scallions, narutomaki.",
-        tag: "most popular"
-    },
-    {
-        image: "./assets/menu/TomatoEggDropSoup.jpg",
-        name: "Egg drop soup with tomato",
-        price: "5.50€",
-        description: "Egg drop soup with tomato scallions and coriander.",
-        tag: "starters"
-    },
-    {
-        image: "./assets/menu/Pan-fried-vegetarian-dumplings.jpg",
-        name: "Pan-fried vegetarian dumplings",
-        price: "5.50€",
-        description: "Vegetarian dumplings with spinach, Pak Choi, carrot, shiitake mushroom, noodles, scallions, ginger, egg.",
-        tag: "starters"
-    },
-    {
-        image: "./assets/menu/Shrimp-yakisoba.jpg",
-        name: "Shrimp Yakisoba",
-        price: "8.80€",
-        description: "Soba noodles with shrimps, onion, carrot, shiitake mushrooms, scallions and cabbage leaves.",
-        tag: "main dishes"
-    },
-    {
-        image: "./assets/menu/miso-katsu-ramen.jpg",
-        name: "Miso Katsu Ramen",
-        price: "8.90€",
-        description: "Miso noodles with pork broth, breaded pork, hard-boiled egg, scallions and narutomaki.",
-        tag: "main dishes"
-    },
-    {
-        image: "./assets/menu/japanese_beef_yakisoba.jpeg",
-        name: "Beef Yakisoba",
-        price: "8.80€",
-        description: "Soba noodles with beef, mushrooms, snap peas, ginger and scallions.",
-        tag: "main dishes"
-    },
-    {
-        image: "./assets/menu/bean-sprouts.jpg",
-        name: "Stir-fried Mung bean sprouts",
-        price: "8.50€",
-        description: "Veggie-friendly strir-fried mung bean sprouts with soy sauce, garlic, fresh chili, scallions and Sichuan peppercorns.",
-        tag: "main dishes"
-    },
-    {
-        image: "./assets/menu/cold-brewed-green-tea.jpg",
-        name: "Cold Brewed Green Tea",
-        price: "2€",
-        description: "Cold brew sencha green tea. Served without sugar.",
-        tag: "drinks"
-    },
-    {
-        image: "./assets/menu/water.jpg",
-        name: "Water",
-        price: "2€",
-        description: "Need water, nothing more, nothing less? We have that too. High quality water, bottled in Belgium.",
-        tag: "drinks"
-    },
-    {
-        image: "./assets/menu/Wine.jpg",
-        name: "Wine of the day",
-        price: "25€",
-        description: "Make yourself a favor and taste one of the high quality wines carfully chosen by our specialist here at Omagi.",
-        tag: "drinks"
-    },
-    {
-        image: "./assets/menu/soda.jpg",
-        name: "Soda",
-        price: "2.50€",
-        description: "We have a big choice of soda ranging from Coke, Fanta and Sprite (diet or regular) to Japanese original Ramune soda. Please add to desired amount to the basket and precise your choice at checkout.",
-        tag: "drinks"
-    },
-    {
-        image: "./assets/menu/pineapple-buns.jpg",
-        name: "Pineapple Buns",
-        price: "1€",
-        description: "Crispy and sweet pineapple pastry from Hong Kong. Sold individually.",
-        tag: "desserts"
-    },
-    {
-        image: "assets/menu/tricolor-dango.jpg",
-        name: "Tricolor Dango",
-        price: "1.50€",
-        description: "Japanese traditional sweetened rice flour dumplings, with natural food coloring",
-        tag: "desserts"
-    }
-];
 
 function filterMenu(pattern){
-    const recherche = []
-    for (let element of menu){
-        if(element.name.match(pattern)||element.description.match(pattern)|| element.tag.match(pattern)){
+    const recherche = [] 
+    for (let element of collection){
+        if(element.name.match(pattern)||element.description.match(pattern) || element.tag.name.match(pattern)){
         recherche.push(element)
         }
     }
-    const rechercheDiv = document.querySelector('')
+    
+
+    const rechercheDiv = document.querySelector("body > div:nth-child(5)")
     rechercheDiv.innerHTML=''
+    
+    const foodContainer = document.createElement('div');
+    foodContainer.className = "container";
+    rechercheDiv.appendChild(foodContainer); 
+
+    
+    const foodRowCols = document.createElement('div');
+    foodRowCols.className = 'columns is-multiline is-centered mb-0';
+    foodContainer.appendChild(foodRowCols);
 
     for (let element of recherche){
 
-
-
-
+        const foodCol = document.createElement('div');
+        foodCol.className = "column is-full-mobile is-half-tablet is-half-desktop is-narrow";
+        foodRowCols.appendChild(foodCol);
+    
+        const foodCard = document.createElement('div');
+        foodCard.className = "card";
+        foodCol.appendChild(foodCard);
+    
+        const foodImgDiv = document.createElement('div');
+        foodImgDiv.className = "card-image";
+        foodCard.appendChild(foodImgDiv);
+    
+        const foodFigure = document.createElement('figure');
+        foodFigure.className = "image is-5by4";
+        foodImgDiv.appendChild(foodFigure);
+    
+        const foodImg = document.createElement('img');
+        foodImg.src = element.image;
+        foodImg.alt = element.name;
+        foodFigure.appendChild(foodImg);
+    
+        const foodCardContent = document.createElement('div');
+        foodCardContent.className = "card-content"
+        foodCard.appendChild(foodCardContent);
+    
+        const foodMedia = document.createElement('div');
+        foodMedia.className = "media"
+        foodCardContent.appendChild(foodMedia);
+    
+        const foodMediaContent = document.createElement('div');
+        foodMediaContent.className = "media-content"
+        foodMedia.appendChild(foodMediaContent);
+    
+        const foodTitle = document.createElement('p');
+        foodTitle.className = "title is-4";
+        foodTitle.innerHTML = element.name;
+        foodMediaContent.appendChild(foodTitle);
+    
+        const foodSubtitle = document.createElement('p');
+        foodSubtitle.className = "subtitle is-12";
+        foodSubtitle.innerHTML = element.description;
+        foodMediaContent.appendChild(foodSubtitle);
+    
+        const foodFooter = document.createElement('footer')
+        foodFooter.className = "card-footer"
+        foodMediaContent.appendChild(foodFooter);
+    
+        const foodTag = document.createElement('span');
+        foodTag.className = element.tag.class + " is-rounded is-medium";
+        foodTag.innerHTML = element.tag.name;
+        foodFooter.appendChild(foodTag);
+    
+        if (element.veg) {
+            const vegBasket = document.createElement('i');
+            vegBasket.className = "fas fa-leaf";
+            vegBasket.style.color = "green"
+            foodFooter.appendChild(vegBasket);
+        }
+    
+        const foodAnchor = document.createElement('a');
+        foodAnchor.href = ""
+        foodFooter.appendChild(foodAnchor);
+    
+        const foodBasket = document.createElement('i');
+        foodBasket.className = "fas fa-shopping-basket";
+        foodAnchor.appendChild(foodBasket);
+    
     }
-}
-
-document.querySelector('searchBar').addEventListener('keyup',input=>{
-    filterMenu(input.target.value)
+    }
+document.querySelector("#searchBar").addEventListener('keyup',input=>{
+    const inputValue = input.target.value
+    const pattern = new RegExp(inputValue, 'gim')
+    filterMenu(pattern)
+    
 })
